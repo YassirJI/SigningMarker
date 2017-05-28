047b6bc9fb3c304717c71894b4608042a3f6c975
YJi60967819 <janati.idrissi.yassir@gmail.com>
2017-05-28 15:15:03 +0000

    Signature tag

--- src/app/document-signing-creation/document-signing-creation.component.ts
+++ src/app/document-signing-creation/document-signing-creation.component.ts
@@ -1,22 +1,64 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input} from '@angular/core';
 import { Tab} from './tab';
 
 import * as $ from 'jquery';
+import SignaturePad from 'signature_pad';
+import * as html2canvas from "html2canvas";
+
 @Component({
   selector: 'app-document-signing-creation',
   templateUrl: './document-signing-creation.component1.html',
   styleUrls: ['./document-signing-creation.component.css'],
 })
-export class DocumentSigningCreationComponent implements OnInit {
+export class DocumentSigningCreationComponent implements OnInit, AfterViewInit {
   fileSource:String;
 
   private draggedItem:Element;
   private offsetX:number;
   private offsetY:number;
   
+  private signHereIncrement:number = 1;
+  private activeSignatureTagId:string;
+  private activeTypedSignerName:string = "Yassir Janati";
+  
+  private signaturePad:SignaturePad;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    var signCanvas = document.querySelector("#signature-pad");
+    this.signaturePad = new SignaturePad(signCanvas, {backgroundColor: 'rgba(255, 255, 255, 0)', penColor: 'rgb(0, 0, 0)'});
+  }
+
+  saveSignature():void {
+
+    let image;
+    let jThis = this;
+
+    if($("#drawIt").is(".active")){
+        image = this.signaturePad.toDataURL('image/png');
+        jThis.updatePeronalizedSignatureTag(jThis, image);
+    } else {
+        html2canvas(document.querySelector("#divtypeName")).then(function(canvas) {
+            image = canvas.toDataURL('image/png');
+            jThis.updatePeronalizedSignatureTag(jThis, image);
+            });
+    }
+  }
+
+  updatePeronalizedSignatureTag(jThis, image):void {
+          let tagDivElement = $("#" + this.activeSignatureTagId + "");
+          tagDivElement.css('background-image', 'url("' + image + '")');
+          tagDivElement.css('background-color', 'white'); 
+          tagDivElement.width(180);
+          tagDivElement.height(60);   
+  }
+
+  clearCanvasSignature():void {
+    this.signaturePad.clear();
   }
 
   onDrag(draggEvent:DragEvent):void{
@@ -59,13 +101,19 @@ export class DocumentSigningCreationComponent implements OnInit {
        
        let newElement;
        if (element.hasClass("signature-tool")) {
-          newElement =  $("<div draggable class='signerTag signHereTag draggable' recipientId='0'  order='0' tagtype='signHere' tagcolor='tagTopYellow' style='position: absolute; left:" + offsetXPos + "px; top:" + offsetYPos + "px;background-image:url(../../assets/sign_here.png);background-repeat:no-repeat;background-position:center;background-size: contain;height:35px;width:160px;background-color: antiquewhite;cursor: move; border: 1px solid #BEBEBE; margin: 0 5px 10px 0;' clonedtag='yes' id='dragTagDiv'></div>");
+          newElement =  $("<div draggable='true' data-toggle='modal' data-target='#signatureModal' class='signerTag signHereTag draggable' id='signTagDiv"+this.signHereIncrement+"' signTagId='"+this.signHereIncrement+"' recipientId='0'  order='0' tagtype='signHere' tagcolor='tagTopYellow' style='position: absolute; left:" + offsetXPos + "px; top:" + offsetYPos + "px;background-image:url(../../assets/sign_here.png);background-repeat:no-repeat;background-position:center;background-size: contain;height:35px;width:160px;background-color: antiquewhite;cursor: move; border: 1px solid #BEBEBE; margin: 0 5px 10px 0;' clonedtag='yes' id='dragTagDiv'></div>");
+          let jThis = this;
+          newElement.click(function (evt) {
+              jThis.activeSignatureTagId = this.id;
+              jThis.clearCanvasSignature();
+          });
+          this.signHereIncrement++;
        }
        else if (element.hasClass("text-tool")) {
-          newElement = $("<div draggable class='signerTag nameHereTag draggable' recipientId='0'  order='0' tagtype='text' tagcolor='tagTopYellow' style='position: absolute; left:" + offsetXPos + "px; top:" + offsetYPos + "px; background-image:url(../../assets/text.png);background-repeat:no-repeat;background-position:center;background-size: contain;height:35px;width:160px;background-color: antiquewhite;cursor: move; border: 1px solid #BEBEBE; margin: 0 5px 10px 0;' clonedtag='yes' id='dragTagDiv'></div>");
+          newElement = $("<div draggable='true' class='signerTag nameHereTag draggable' recipientId='0'  order='0' tagtype='text' tagcolor='tagTopYellow' style='position: absolute; left:" + offsetXPos + "px; top:" + offsetYPos + "px; background-image:url(../../assets/text.png);background-repeat:no-repeat;background-position:center;background-size: contain;height:35px;width:160px;background-color: antiquewhite;cursor: move; border: 1px solid #BEBEBE; margin: 0 5px 10px 0;' clonedtag='yes' id='dragTagDiv'></div>");
        }
        else if (element.hasClass("date-tool")) {
-          newElement =  $("<div draggable class='signerTag nameHereTag draggable' recipientId='0'  order='0' tagtype='dateSigned' tagcolor='tagTopYellow' style='position: absolute; left:" + offsetXPos + "px; top:" + offsetYPos + "px;background-image:url(../../assets/signed_date.png);background-repeat:no-repeat;background-position:center;background-size: contain;height:35px;width:160px;background-color: antiquewhite;cursor: move; border: 1px solid #BEBEBE; margin: 0 5px 10px 0;' clonedtag='yes' id='dragTagDiv'></div>");
+          newElement =  $("<div draggable='true' class='signerTag nameHereTag draggable' recipientId='0'  order='0' tagtype='dateSigned' tagcolor='tagTopYellow' style='position: absolute; left:" + offsetXPos + "px; top:" + offsetYPos + "px;background-image:url(../../assets/signed_date.png);background-repeat:no-repeat;background-position:center;background-size: contain;height:35px;width:160px;background-color: antiquewhite;cursor: move; border: 1px solid #BEBEBE; margin: 0 5px 10px 0;' clonedtag='yes' id='dragTagDiv'></div>");
        }
        newElement.bind("dragstart", function(event) {
          return jThis.onDrag(<DragEvent>event.originalEvent);
@@ -116,7 +164,6 @@ export class DocumentSigningCreationComponent implements OnInit {
 
   public fileSourceChanged(event:Event):void{
     this.fileSource=event.srcElement.nodeValue;
-    console.log(event.type);
     let input =<HTMLInputElement> document.getElementById('fileinput');
         if (!input) {
             console.log("p", "Um, couldn't find the fileinput element.");
